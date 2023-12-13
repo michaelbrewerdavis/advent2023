d419bf130ab4562e0f5fec6ea559bf4115d63f0c
Michael Brewer-Davis <mbrewerdavis@outschool.com>
2023-12-13 01:48:40 -0600

    day13

--- day13.exs
+++ day13.exs
@@ -17,25 +17,27 @@ defmodule Day13 do
     end)
   end
 
-  def find_split_in_one_direction(strings) do
-    IO.inspect(strings)
+  def differing_points_at_n(strings, n) do
+    0..tuple_size(strings)
+    |> Stream.flat_map(fn delta ->
+      if n + delta < tuple_size(strings) and n - delta - 1 >= 0 do
+        Enum.zip(elem(strings, n + delta), elem(strings, n - delta - 1))
+        |> Enum.with_index()
+        |> Enum.map(fn {{a, b}, index} -> if a != b, do: n, else: nil end)
+        |> Enum.filter(&(&1 != nil))
+      else
+        []
+      end
+    end)
+  end
 
+  def find_mirror_in_one_direction(strings) do
     1..(tuple_size(strings) - 1)
     |> Enum.find(fn n ->
-      IO.inspect({"checking whether mirrors above n", n})
-
-      0..tuple_size(strings)
-      |> Enum.all?(fn delta ->
-        if n + delta < tuple_size(strings) and n - delta - 1 >= 0 do
-          IO.inspect(elem(strings, n + delta))
-          IO.inspect(elem(strings, n - delta - 1))
-
-          (elem(strings, n + delta) == elem(strings, n - delta - 1))
-          |> IO.inspect()
-        else
-          true
-        end
-      end)
+      differing_points_at_n(strings, n)
+      # |> Enum.empty?()
+      |> Enum.take(2)
+      |> (&(Enum.count(&1) == 1)).()
     end)
   end
 
@@ -43,26 +45,29 @@ defmodule Day13 do
     row_strings =
       box
       |> List.to_tuple()
-      |> find_split_in_one_direction()
+      |> find_mirror_in_one_direction()
   end
 
   def find_split_y(box) do
     col_strings =
       Stream.zip(box)
+      |> Stream.map(&Tuple.to_list/1)
       |> Enum.to_list()
       |> List.to_tuple()
-      |> find_split_in_one_direction()
+      |> find_mirror_in_one_direction()
   end
 
   def find_split(box) do
-    x =
-      (find_split_y(box) ||
-         find_split_x(box) * 100)
-      |> IO.inspect()
+    find_split_y(box) ||
+      find_split_x(box) * 100
   end
 
   def main(f) do
-    parse_input(f) |> Enum.map(&find_split/1) |> IO.inspect() |> Enum.sum()
+    parse_input(f)
+    # |> Enum.take(1)
+    |> Enum.map(&find_split/1)
+    |> IO.inspect()
+    |> Enum.sum()
   end
 end
 
